454d865946d6b984c79da5a84bf81f42c38ed55b
Lucas Baiz <lbaiz@gricciardi.com.ar>
2024-07-31 21:12:00 -0300

    cambio

--- appweb/app/forms.py
+++ appweb/app/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class formulario(forms.Form):
     
     #especificar los campos
-    servicios = forms.CharField()
+    servicio = forms.CharField()
     camada = forms.IntegerField()
